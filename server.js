a8684b12e87bd754fb1941047a25d8bb0f2b09a9
Padovan <victorpadovan1997@gmail.com>
2020-03-29 18:46:27 -0300

    Relação Produto Vendedor

---
+++ server.js
@@ -0,0 +1,34 @@
+// import express from 'express';
+// import bodyParser from 'body-parser';
+var express = require ('express');
+var bodyParser = require('body-parser');
+const app = express();
+var mongoose = require('mongoose');
+
+//PERSISTÊNCIA
+mongoose.connect('mongodb+srv://victor:victorpadovan1997@cluster0-waiml.mongodb.net/bd-teste20?retryWrites=true&w=28015', 
+{useNewUrlParser:true, useUnifiedTopology: true }
+);
+
+//Configuração do server para usar body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Definindo a porta via arquivo de configuração
+var port = process.env.port || 3000;
+
+//ROTAS
+var indexRoute = require("./routes/index-routes");
+var productRoute = require("./routes/product-routes");
+
+//Vincular a aplicacao (app) com o motor de rotas
+//Rota geral (teste)
+app.use('/api', indexRoute);
+//Rotas para produtos
+app.use('/api/products', productRoute);
+
+app.listen(port, () => {
+    console.log('Server up and running!');
+
+});
+
