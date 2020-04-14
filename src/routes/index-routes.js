8880ae85a4cbbfe4c3d575cefc489b1dedeef5dc
marcusLSantos <marcus.santos@univem.edu.br>
2020-04-14 23:17:45 +0000

    comit miidl

--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -3,7 +3,7 @@ var router = express.Router(); //interceptação das rotas
 
 router.use(function (req, res, next){
     //Aqui implementaremos rotinas de LOGs, Validações, Autenticações...
-    console.log("Ip da pessoa", req.ip);
+    console.log("Ip da pessoa", req.ip); 
     next();
 });
 
